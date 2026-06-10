2c35ef0d5493b512be7aa1792e619b7c9adea83a
41937954X <edson.fatec8@gmail.com>
2026-06-10 12:01:42 -0300

    fix: ajusta base condicional nativa do vite para o deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Se for o comando 'npm run build' (no GitHub), usa o caminho relativo './'
-  // Se for o comando 'npm run dev' (no localhost), usa a raiz limpa '/'
-  base: process.env.NODE_ENV === 'production' ? './' : '/',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // Se o comando for 'build' (no GitHub Actions), ele usa o caminho do repositório.
+    // Se for qualquer outro comando (como 'dev' no localhost), usa a raiz '/'.
+    base: command === 'build' ? '/Bol-o-da-Copa/' : '/',
+  }
 })
